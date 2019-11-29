eafe173cef0f1103d6582c71869176b117c577a7
zijiepei <peizijie2@gmail.com>
2019-11-29 23:06:20 +0800

    d1p4

---
+++ d2p1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int d2p1()
+{
+	int temp;
+	int x = 6;
+	int y = 7;
+	printf("x = %d\n,y = %d\n", x, y);
+	temp = x;
+	x = y;
+	y = temp;
+	printf("x = %d\n,y = %d\n", x, y);
+	system("pause");
+	return 0;
+
+	
+
+
+
+
+
+
+
+
+}
