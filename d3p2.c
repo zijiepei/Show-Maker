19c9886128de95e98ecef136302933198bcec3dc
zijiepei <peizijie2@gmail.com>
2019-12-22 23:23:25 +0800

    d3p2.c

---
+++ d3p2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int d3p2()
+{
+	int i;
+	double sum = 0;
+	double tmp = 0;
+	int flag = 1;
+	for (i = 1; i <= 100; i++)
+	{
+		tmp = flag * 1.0 / i;
+		flag *= -1;
+		sum += tmp;
+	}
+	printf("%.4lf\n", sum);
+	system("pause");
+	return 0;
+
+
+
+
+
+
+
+}
